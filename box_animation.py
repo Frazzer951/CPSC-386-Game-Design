c21d529299d3616c7e257368e14b4dda92693634
Frazzer951 <luke343279@gmail.com>
2022-08-29 21:43:12 -0700

    update to use vector

--- box_animation.py
+++ box_animation.py
@@ -5,20 +5,25 @@ import time
 
 class Vector:
     def __init__(self, vx, vy):
-        self.vx = vx
-        self.vy = vy
+        self.vx, self.vy = vx, vy
 
     def __repr__(self):
         return f"Vector({self.vx}, {self.vy})"
 
     def __add__(self, other):
-        return Vector(vx=self.vx + other.vx, vy=self.vy + other.vy)
+        return Vector(self.vx + other.vx, self.vy + other.vy)
 
     def __sub__(self, other):
-        return Vector(vx=self.vx - other.vx, vy=self.vy - other.vy)
+        return self + -other
 
     def __neg__(self):
-        return Vector(vx=-self.vx, vy=-self.vy)
+        return Vector(-self.vx, -self.vy)
+
+    def __mul__(self, k):
+        return Vector(k * self.vx, k * self.vy)
+
+    def __rmul__(self, k):
+        return self.__mul__(k)
 
     def __eq__(self, other):
         return self.vx == other.vx and self.vy == other.vy
@@ -30,6 +35,8 @@ class Vector:
 
         vsum = v1 + v2
         vsub = v1 - v2
+        vmul = v1 * 3
+        vmulr = 3 * v1
         neg_v1 = -v1
         neg_v2 = -v2
 
@@ -37,22 +44,12 @@ class Vector:
         print(f"{v1} - {v2} = {vsub}")
         print(f"-{v1} = {neg_v1}")
         print(f"-{v2} = {neg_v2}")
+        print(f"3 * {v1} = {vmulr}")
+        print(f"{v1} * 3 = {vmul}")
 
-        assert vsum == Vector(vx=10, vy=5)
-        assert vsub == Vector(vx=10, vy=-5)
-        assert neg_v1 == Vector(vx=-10, vy=0)
-        assert neg_v2 == Vector(vx=0, vy=-5)
-
-
-# Set up direction variables.
-DOWNLEFT = "downleft"
-DOWNRIGHT = "downright"
-UPLEFT = "upleft"
-UPRIGHT = "upright"
 
 MOVESPEED = 4
 
-# Set up the colors.
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -60,84 +57,51 @@ BLUE = (0, 0, 255)
 
 
 def play():
-    # Set up pygame.
     pygame.init()
 
-    # Set up the window.
     WINDOWWIDTH = 400
     WINDOWHEIGHT = 400
     windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
     pygame.display.set_caption("Animation")
 
-    # Set up the box data structure.
-    b1 = {"rect": pygame.Rect(300, 80, 50, 100), "color": RED, "dir": UPRIGHT}
-    b2 = {"rect": pygame.Rect(200, 200, 20, 20), "color": GREEN, "dir": UPLEFT}
-    b3 = {"rect": pygame.Rect(100, 150, 60, 60), "color": BLUE, "dir": DOWNLEFT}
+    b1 = {"rect": pygame.Rect(300, 80, 50, 100), "color": RED, "dir": Vector(1, -1)}
+    b2 = {"rect": pygame.Rect(200, 200, 20, 20), "color": GREEN, "dir": Vector(-1, -1)}
+    b3 = {"rect": pygame.Rect(100, 150, 60, 60), "color": BLUE, "dir": Vector(-1, 1)}
     boxes = [b1, b2, b3]
 
-    # Run the game loop.
     while True:
-        # Check for the QUIT event.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-        # Draw the white background onto the surface.
         windowSurface.fill(WHITE)
 
         for b in boxes:
-            # Move the box data structure.
-            if b["dir"] == DOWNLEFT:
-                b["rect"].left -= MOVESPEED
-                b["rect"].top += MOVESPEED
-            if b["dir"] == DOWNRIGHT:
-                b["rect"].left += MOVESPEED
-                b["rect"].top += MOVESPEED
-            if b["dir"] == UPLEFT:
-                b["rect"].left -= MOVESPEED
-                b["rect"].top -= MOVESPEED
-            if b["dir"] == UPRIGHT:
-                b["rect"].left += MOVESPEED
-                b["rect"].top -= MOVESPEED
+            rect = b["rect"]
+            vel = b["dir"]
+            posn = Vector(rect.left, rect.top)
+            posn = posn + MOVESPEED * vel
 
-            # Check whether the box has moved out of the window.
-            if b["rect"].top < 0:
-                # The box has moved past the top.
-                if b["dir"] == UPLEFT:
-                    b["dir"] = DOWNLEFT
-                if b["dir"] == UPRIGHT:
-                    b["dir"] = DOWNRIGHT
-            if b["rect"].bottom > WINDOWHEIGHT:
-                # The box has moved past the bottom.
-                if b["dir"] == DOWNLEFT:
-                    b["dir"] = UPLEFT
-                if b["dir"] == DOWNRIGHT:
-                    b["dir"] = UPRIGHT
-            if b["rect"].left < 0:
-                # The box has moved past the left side.
-                if b["dir"] == DOWNLEFT:
-                    b["dir"] = DOWNRIGHT
-                if b["dir"] == UPLEFT:
-                    b["dir"] = UPRIGHT
-            if b["rect"].right > WINDOWWIDTH:
-                # The box has moved past the right side.
-                if b["dir"] == DOWNRIGHT:
-                    b["dir"] = DOWNLEFT
-                if b["dir"] == UPRIGHT:
-                    b["dir"] = UPLEFT
+            b["rect"].left = posn.vx
+            b["rect"].top = posn.vy
 
-            # Draw the box onto the surface.
+            if b["rect"].top < 0 or b["rect"].bottom > WINDOWHEIGHT:
+                vel = Vector(vel.vx, -vel.vy)
+            if b["rect"].left < 0 or b["rect"].right > WINDOWWIDTH:
+                vel = Vector(-vel.vx, vel.vy)
+
+            b["dir"] = vel
+
             pygame.draw.rect(windowSurface, b["color"], b["rect"])
 
-        # Draw the window onto the screen.
         pygame.display.update()
         time.sleep(0.02)
 
 
 def main():
-    # play()
-    Vector.run_tests()
+    play()
+    # Vector.run_tests()
 
 
 if __name__ == "__main__":
